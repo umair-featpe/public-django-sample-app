502f4493d428e67fe21c84563df6977419bdb7df
umair-featpe <post.cloudcontainer@gmail.com>
2024-12-25 01:39:00 +0530

    Update settings.py

--- django_sample_project/settings.py
+++ django_sample_project/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECRET_KEY = "12345-hardcoded-secret"
+DEBUG = True  # Intentionally set to True for vulnerability
+
